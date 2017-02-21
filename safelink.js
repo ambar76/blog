534be1bb7859e5ab012d3d2708f4ced8894cf7f1
ambar76 <ambarmasadepan@gmail.com>
2017-02-21 15:34:29 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -1,40 +1,21 @@
-(function() {
-    var v = document.getElementsByClassName("youtube-player");
-    for (var n = 0; n < v.length; n++) {
-        var p = document.createElement("div");
-        p.innerHTML = JWTubeThumb(v[n].dataset.id);
-        p.onclick = JWTubeIframe;
-        v[n].appendChild(p);
-    }
-})();
- 
-function JWTubeThumb(id) {
-    return '<img class="youtube-thumb" src="https://i.ytimg.com/vi/' + id + '/hqdefault.jpg"><div class="play-button"></div>               <div class='share-box'>
-                 
-                <div class='share-art'>
-
-                  <div class='sa-title'>
-                    <strong>SHARE THIS</strong>
-                  </div>
-
-                  <a class='facebook' expr:href='&quot;http://www.facebook.com/sharer.php?u=&quot; + data:post.url + &quot;&amp;title=&quot;+ data:post.title' onclick='window.open(this.href, &apos;windowName&apos;, &apos;width=600, height=400, left=24, top=24, scrollbars, resizable&apos;); return false;' rel='nofollow' target='_blank'><i class='fa fa-facebook'/><span> Share it</span></a>
-
-                  <a class='twitter' expr:href='&quot;http://twitter.com/share?url=&quot; + data:post.url + &quot;&amp;title=&quot; + data:post.title' onclick='window.open(this.href, &apos;windowName&apos;, &apos;width=600, height=400, left=24, top=24, scrollbars, resizable&apos;); return false;' rel='nofollow' target='_blank'><i class='fa fa-twitter'/><span> Tweet</span></a>
-
-                  <a class='googleplus' expr:href='&quot;https://plus.google.com/share?url=&quot; + data:post.url + &quot;&amp;title=&quot; + data:post.title' onclick='window.open(this.href, &apos;windowName&apos;, &apos;width=600, height=400, left=24, top=24, scrollbars, resizable&apos;); return false;' rel='nofollow' target='_blank'><i class='fa fa-google-plus'/><span> Share it</span></a>
-
-                  <a class='linkedin' expr:href='&quot;http://www.linkedin.com/shareArticle?url=&quot; + data:post.url + &quot;&amp;title=&quot;+ data:post.title' onclick='window.open(this.href, &apos;windowName&apos;, &apos;width=600, height=400, left=24, top=24, scrollbars, resizable&apos;); return false;' rel='nofollow' target='_blank'><i class='fa fa-linkedin'/><span> Share it</span></a>
-
-                  <a class='pinterest' expr:href='&quot;http://pinterest.com/pin/create/button/?url=&quot; + data:post.url + &quot;&amp;media=&quot; + data:post.firstImageUrl + &quot;&amp;description=&quot; + data:post.snippet' onclick='window.open(this.href, &apos;windowName&apos;, &apos;width=600, height=400, left=24, top=24, scrollbars, resizable&apos;); return false;' rel='nofollow' target='_blank'><i class='fa fa-pinterest-p'/><span> Pin it</span></a>  
-
-                </div>
-               </div>';
-}
- 
-function JWTubeIframe() {
-    var iframe = document.createElement("iframe");
-    iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.parentNode.dataset.id + "?autoplay=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&controls=0&showinfo=0");
-    iframe.setAttribute("frameborder", "0");
-    iframe.setAttribute("id", "youtube-iframe");
-    this.parentNode.replaceChild(iframe, this);
-}
+	(function() {
+	    var v = document.getElementsByClassName("youtube-player");
+	    for (var n = 0; n < v.length; n++) {
+	        var p = document.createElement("div");
+	        p.innerHTML = JWTubeThumb(v[n].dataset.id);
+	        p.onclick = JWTubeIframe;
+	        v[n].appendChild(p);
+	    }
+	})();
+	 
+	function JWTubeThumb(id) {
+	    return '<img class="youtube-thumb" src="http://i.ytimg.com/vi/' + id + '/hqdefault.jpg"><div class="play-button"></div>';
+	}
+	 
+	function JWTubeIframe() {
+	    var iframe = document.createElement("iframe");
+	    iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.parentNode.dataset.id + "?autoplay=1& modestbranding=1&rel=0&hl=id");
+	    iframe.setAttribute("frameborder", "0");
+	    iframe.setAttribute("id", "youtube-iframe");
+	    this.parentNode.replaceChild(iframe, this);
+	}
